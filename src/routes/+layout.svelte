ac260bb9ed817106e0b4d56f962b16842b95d4e3
jconan <conanjahh@hotmail.com>
2023-10-17 21:29:35 +0100

    add style and reset css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import '../app.css';
+	import 'the-new-css-reset/css/reset.css';
+</script>
+
+<header>
+	<ul>
+		<li>
+			<a href="/">Maison</a>
+		</li>
+	</ul>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	ul {
+		margin-bottom: 3rem;
+		width: 500px;
+		display: flex;
+
+		& li {
+			margin-right: 20px;
+			font-size: 26px;
+			text-decoration: underline;
+
+			&:hover {
+				font-weight: bold;
+			}
+		}
+	}
+</style>
