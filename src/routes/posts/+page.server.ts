e9286158dc3c59dd3718771c9d91cc17becddcda
jconan <conanjahh@hotmail.com>
2023-10-22 20:55:16 +0100

    use zod parse json request

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -7,11 +7,18 @@ const PostSchema = z.object({
     body: z.string()
 })
 
-type Post = z.infer<typeof PostSchema>
+const PostArraySchema = z.array(PostSchema)
+
+// faire une inférence pour déterminer le type a partir du schema
+// type Post = z.infer<typeof PostSchema>
 
 export async function load({ fetch }) {
-    const posts: Post[] = await (await fetch("https://jsonplaceholder.typicode.com/posts")).json()
+    // const posts: Post[] = await (await fetch("https://jsonplaceholder.typicode.com/posts")).json()
+
+    const posts = PostArraySchema.parse(await (await fetch("https://jsonplaceholder.typicode.com/posts")).json())
+
     return {
         posts
     }
 }
+
